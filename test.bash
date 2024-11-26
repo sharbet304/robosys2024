7b1fb439ba2dc4aea3c33c757266da9e4afe2d14
Daiki Ogawa <daguixiaochuan682@gmail.com>
2024-11-26 10:16:16 +0900

    henkou

--- test.bash
+++ test.bash
@@ -1,12 +1,34 @@
-#!/bin/bash
-test.bashtest.bash#!/bin/bash
+#!/bin/bash -xv
 
+# SPDX-FileCopyrightText: 2024 Daiki Ogawa 
+# SPDX-License-Identifier: BSD-3-Clause
 ng(){
 	echo ${1}行目が違うよ
 	res=1
 }
 res=0
-a=山田
-[ "$a" = 山田 ] || ng "$LINENO"
 
+#なんもないとき
+
+out=$(echo -n | ./ohana)
+[ "$out" = "なんか入れて" ] || ng "$LINENO"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+[ "${res}" = 0 ] && echo OK
 exit $res

1feff6e68af6ad6c2657d477111c8153dd31223c
Daiki Ogawa <daguixiaochuan682@gmail.com>
2024-11-26 10:51:30 +0900

    henkou

--- test.bash
+++ test.bash
@@ -12,9 +12,84 @@ res=0
 out=$(echo -n | ./ohana)
 [ "$out" = "なんか入れて" ] || ng "$LINENO"
 
-#春とか
+#春red
 out=$(./ohana 春 赤)
 [ "$out" != *"ヘビイチゴ"* ] || ng "$LINENO"
+
+#春blue
+out=$(./ohana 春 blue)
+[ "$out" != *"デルフィニウム"* ] || ng "$LINENO"
+
+#haruyellow
+out=$(./ohana 春 yellow)
+[ "$out" != *"タンポポ"* ] || ng "$LINENO"
+
+#春white
+out=$(./ohana 春 white)
+[ "$out" != *"マーガレット"* ] || ng "$LINENO"
+
+#natu 
+out=$(./ohana natu aka)
+[ "$out" != *"ランタナ"* ] || ng "$LINENO"
+
+#natu
+out=$(./ohana natu ao)
+[ "$out" != *"朝顔"* ] || ng "$LINENO"
+
+#natu
+out=$(./ohana natu siro)
+[ "$out" != *"日々草"* ] || ng "$LINENO"
+
+#natu
+out=$(./ohana natu ki)
+[ "$out" != *"ひまわり"* ] || ng "$LINENO"
+
+#aki
+out=$(./ohana aki red)
+[ "$out" != *"ダリア"* ] || ng "$LINENO"
+
+#aki
+out=$(./ohana aki blue)
+[ "$out" != *"コルチカム"* ] || ng "$LINENO"
+
+#aki
+out=$(./ohana fall white)
+[ "$out" != *"コダチダリア"* ] || ng "$LINENO"
+
+#aki
+out=$(./ohana autumn yellow)
+[ "$out" != *"マリーゴールド"* ] || ng "$LINENO"
+
+#fuyu
+out=$(./ohana winter aka)
+[ "$out" != *"シクラメン"* ] || ng "$LINENO"
+
+#fuyu
+out=$(./ohana 冬 ao)
+[ "$out" != *"バンジー"* ] || ng "$LINENO"
+
+#fuyu
+out=$(./ohana fuyu siro)
+[ "$out" != *"バンジー"* ] || ng "$LINENO"
+
+#fuyu
+out=$(./ohana fuyu ki)
+[ "$out" != *"バンジー"* ] || ng "$LINENO"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
